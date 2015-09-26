29428882ccf691d07316dda94c8eaa7c9464b5b4
Fong <fong.fan.90@gmail.com>
2015-09-26 02:22:02 -0500

    basics configured

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,45 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
+  });  
+
+  Template.body.events({
+    "submit .new-task": function(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.task.events({
+    "click .toggle-checked": function (){
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
     }
-  });
+  })
+
 }
 
 if (Meteor.isServer) {
