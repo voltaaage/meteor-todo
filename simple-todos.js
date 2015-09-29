0276bfad7b7d786893b1a9bed41ddbda88627b2e
Fong <fong.fan.90@gmail.com>
2015-09-29 01:14:25 -0500

    publish and subscribe

--- simple-todos.js
+++ simple-todos.js
@@ -1,7 +1,22 @@
 Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("tasks",function(){
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId}
+      ]
+    });
+  })
+}
+
 if (Meteor.isClient) {
   // This code only runs on the client
+
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function() {
       if (Session.get("hideCompleted")){
@@ -40,13 +55,22 @@ if (Meteor.isClient) {
 
   });
 
+  Template.task.helpers({
+    isOwner: function(){
+      return this.owner === Meteor.userId()
+    }
+  });
+
   Template.task.events({
     "click .toggle-checked": function (){
       // Set the checked property to the opposite of its current value
       Meteor.call("setChecked",this._id, ! this.checked);
     },
     "click .delete": function () {
-      Meteor.call("deleteTask",this._id)
+      Meteor.call("deleteTask",this._id);
+    },
+    "click .toggle-private": function (){
+      Meteor.call("setPrivate",this._id, ! this.private);
     }
   });
 
@@ -71,16 +95,28 @@ Meteor.methods({
     });
   },
   deleteTask: function(taskId){
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()){
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.remove(taskId);
   },
   setChecked: function(taskId,setChecked){
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.update(taskId, { $set: {checked: setChecked}})
+  },
+  setPrivate: function(taskId,setToPrivate){
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, {$set: {private: setToPrivate} });
   }
 
 });
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
